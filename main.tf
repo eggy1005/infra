10cb544a8d85c19fccdaf90ad1616f99f36f55cd
Jessy <eggy1005@hotmail.com>
2021-07-06 16:43:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-# terraform{
-#     backend "gcs" {
-#         bucket = "gcp-951c0919-tf-state"
-#         prefix = "terraform/state"
-#     }
-# }
+terraform{
+    backend "gcs" {
+        bucket = "gcp-951c0919-tf-state"
+        prefix = "terraform/state"
+    }
+}
 resource "null_resource" "null" {
     triggers = {
         value = "Doing nothing"
