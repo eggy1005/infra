0fb8938499787b6cad5e3e1c6d35a7478a5e9b2d
eggy1005 <eggy1005@hotmail.com>
2021-07-13 15:31:36 +0100

    add vm

--- main.tf
+++ main.tf
@@ -4,18 +4,12 @@ terraform{
         prefix = "terraform/state"
     }
 }
-# resource "google_service_account" "myaccount" {
-#   account_id   = "jessy-service-acount"
-#   project      =  "g-951c0919"
-#   display_name = "Jessy's Service account for Github action Pipeline"
-# }
+
 
 resource "google_compute_instance" "default" {
-  name         = "test"
-  machine_type = "e2-medium"
+  name         = "jessy-virtual-machine-from-terraform"
+  machine_type = "f1-micro"
   zone         = "us-central1-a"
-
-  tags = ["foo", "bar"]
 
   boot_disk {
     initialize_params {
@@ -23,24 +17,15 @@ resource "google_compute_instance" "default" {
     }
   }
 
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
-
   network_interface {
     network = "default"
 
     access_config {
-      // Ephemeral IP
+      // Include this section to give the VM an external ip address
     }
   }
 
-  metadata = {
-    foo = "bar"
-  }
-
-  metadata_startup_script = "echo hi > /test.txt"
+    metadata_startup_script = "echo hi > /test.txt"
 
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
