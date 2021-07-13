43e3538e71aa646e4b07af969d4d8497988bd2ca
eggy1005 <eggy1005@hotmail.com>
2021-07-13 15:10:32 +0100

    change to my account

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "google_compute_instance" "default" {
 
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
+    email  = google_service_account.myaccount.email
     scopes = ["cloud-platform"]
   }
 }
